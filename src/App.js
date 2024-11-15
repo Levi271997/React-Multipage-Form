2d90f860657dcc7da4ffe2e92d06806f437798d3
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-15 22:52:24 +0800

    structure the main page and sidebar

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
         <main>
-           <div className='container'>
+           
               <div className='form-container'>  
                   <div className='side-bar'>
                     <h1>tset</h1>
                   </div>
               </div>
-           </div>
+           
         </main>
     </div>
   );
