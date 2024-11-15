dc4fea7c86398875e57c7264aa2f1e47861e43e2
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-15 23:08:21 +0800

    sidebar

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
         <main>
            
               <div className='form-container'>  
-                  <div className='side-bar'>
-                    <h1>tset</h1>
-                  </div>
+                <Sidebar/>
               </div>
            
         </main>
@@ -17,3 +15,42 @@ function App() {
 }
 
 export default App;
+
+
+export const Sidebar=()=>{
+  return(
+    <div className='side-bar'>
+          <div className='sidebar-steps'>
+            <div className='sidebar-steps_number active'>1</div>
+            <div className='sidebar-steps_details'>
+                <p className='sidebar-steps_title'>step 1</p>
+                <p className='sidebar-steps_info'>your info</p>
+            </div>
+          </div>
+
+          <div className='sidebar-steps'>
+            <div className='sidebar-steps_number'>2</div>
+            <div className='sidebar-steps_details'>
+                <p className='sidebar-steps_title'>step 2</p>
+                <p className='sidebar-steps_info'>select plan</p>
+            </div>
+          </div>
+
+          <div className='sidebar-steps'>
+            <div className='sidebar-steps_number'>3</div>
+            <div className='sidebar-steps_details'>
+                <p className='sidebar-steps_title'>step 3</p>
+                <p className='sidebar-steps_info'>add-ons</p>
+            </div>
+          </div>
+
+          <div className='sidebar-steps'>
+            <div className='sidebar-steps_number'>4</div>
+            <div className='sidebar-steps_details'>
+                <p className='sidebar-steps_title'>step 4</p>
+                <p className='sidebar-steps_info'>summary</p>
+            </div>
+          </div>
+    </div>
+  )
+}
