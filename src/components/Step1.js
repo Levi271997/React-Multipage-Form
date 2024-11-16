590d1064d80f2eb15542104c0b7d1597cb77c1da
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-16 15:01:38 +0800

    style forms

--- src/components/Step1.js
+++ src/components/Step1.js
@@ -3,7 +3,27 @@ const Step1 = ()=>{
         <>
           <h2 className='form-title'>Personal Info</h2>
           <p className='form-instruct'>Please provide your name, email address, and phone number.</p>
-          </>
+
+          <form>
+                <div className="fieldgroup">
+                    <label>Name</label>
+                    <p className="error-text"></p>
+                    <input typeof="text" name="name" placeholder="e.g. Stephen King"/>
+                </div>    
+
+                <div className="fieldgroup">
+                    <label>Email Address</label>
+                    <p className="error-text"></p>
+                    <input typeof="text" name="name" placeholder="e.g. stephenking@lorem.com"/>
+                </div>    
+
+                 <div className="fieldgroup">
+                    <label>Phone Number</label>
+                    <p className="error-text"></p>
+                    <input typeof="text" name="name" placeholder="e.g. +1 234 567 890"/>
+                </div>            
+          </form>
+        </>
     )
 }
 
