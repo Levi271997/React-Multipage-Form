6becced445afab1486e250f32021504f1da323cd
Levi271997 <117424925+Levi271997@users.noreply.github.com>
2024-11-16 23:37:17 +0800

    form status and form validation

--- src/App.js
+++ src/App.js
@@ -5,24 +5,35 @@ import Step2 from './components/Step2';
 import Step3 from './components/Step3';
 import Finishing from './components/Finishing';
 import Thankyou from './components/Thankyou';
+import { useState } from 'react';
 
 
 
 function App() {
+
+  const [step, setStep] = useState(1);
+
+
+ const handleNextStepSet=()=>{
+    setStep((currentStep)=> (step < 4) ? currentStep + 1 : step);
+ }
+ const handlePrevStepSet=()=>{
+  setStep((currentStep)=> (step > 1) ? currentStep - 1 :  step);
+}
   return (
     <div className="App">
         <main>
            
               <div className='form-container'>  
-                <Sidebar/>
+                <Sidebar stepNumber={step}/>
                 <div className='form-pagewrap'>
               
                  <div className='forms-group'>
-                   <Thankyou/>
+                { RenderForm(step)}
                  </div>
                  <div className='steps-buttons'>
-                   <button className='btn steps-prev'>Go Back</button>
-                   <button className='btn steps-next'>Next Step</button>
+                  {(step > 1 ) ?  <button onClick={handlePrevStepSet} className='btn steps-prev'>Go Back</button> : null}
+                   <button onClick={handleNextStepSet} className='btn steps-next'>{(step !== 4) ? 'Next Step': 'Confirm'}</button>
                  </div>
                 </div>
               </div>
@@ -34,3 +45,21 @@ function App() {
 
 export default App;
 
+function RenderForm(step){
+  switch (step){
+    case 1:
+      return <Step1/>
+   
+    case 2:
+      return <Step2/>
+   
+    case 3:
+      return <Step3/>
+   
+    case 4:
+      return <Finishing/>
+
+    default:
+      return null;
+  }
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,33 @@
 
- const Sidebar=()=>{
+ const Sidebar=(props)=>{
     return(
       <div className='side-bar'>
-            <div className='sidebar-steps'>
-              <div className='sidebar-steps_number active'>1</div>
+            <div className='sidebar-steps' data-steps="1">
+              <div className={`sidebar-steps_number ${props.stepNumber === 1 ? 'active':''}`}>1</div>
               <div className='sidebar-steps_details'>
                   <p className='sidebar-steps_title'>step 1</p>
                   <p className='sidebar-steps_info'>your info</p>
               </div>
             </div>
   
-            <div className='sidebar-steps'>
-              <div className='sidebar-steps_number'>2</div>
+            <div className='sidebar-steps' data-steps="2">
+              <div className={`sidebar-steps_number ${props.stepNumber === 2 ? 'active':''}`}>2</div>
               <div className='sidebar-steps_details'>
                   <p className='sidebar-steps_title'>step 2</p>
                   <p className='sidebar-steps_info'>select plan</p>
               </div>
             </div>
   
-            <div className='sidebar-steps'>
-              <div className='sidebar-steps_number'>3</div>
+            <div className='sidebar-steps' data-steps="3">
+              <div className={`sidebar-steps_number ${props.stepNumber === 3 ? 'active':''}`}>3</div>
               <div className='sidebar-steps_details'>
                   <p className='sidebar-steps_title'>step 3</p>
                   <p className='sidebar-steps_info'>add-ons</p>
               </div>
             </div>
   
-            <div className='sidebar-steps'>
-              <div className='sidebar-steps_number'>4</div>
+            <div className='sidebar-steps' data-steps="4">
+              <div className={`sidebar-steps_number ${props.stepNumber === 4 ? 'active':''}`}>4</div>
               <div className='sidebar-steps_details'>
                   <p className='sidebar-steps_title'>step 4</p>
                   <p className='sidebar-steps_info'>summary</p>
